f63191f1e72b4a5ef0f4e89ee4d5b036a58adb7c
Jaime D. Guevara <jguevara@jaimes-air.boisestate.edu>
2018-09-10 18:29:46 -0600

    Worked on this further

--- assignments/la1-1/super-duper.scm
+++ assignments/la1-1/super-duper.scm
@@ -1,13 +1,17 @@
 ; Scheme copy program
-; (define (copy source)
-
+(define (copy source count)
+    (do ((i 1 (1+ i)))
+        ((i > count))
+        (display source)))
 
 (define (super-duper source count)
     (if (null? source)
-        (number? source)
-            source))
+        source
+        (if (number? source)
+            source
+        (copy source count))))
         
-(display (super-duper 123 2))
+; (display (super-duper 123 2))
 ; (display (super-duper '( ) 2))
-; (display (super-duper '(x) 2))
+(display (super-duper '(x) 2))
 (display "\n")
