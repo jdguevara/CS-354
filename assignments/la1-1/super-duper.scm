de90e224e6ec981505263dbcca4a88ed87915af2
Jaime D. Guevara <jguevara@Jaimes-MacBook-Air.local>
2018-09-17 21:00:20 -0600

    Super-duper is done

--- assignments/la1-1/super-duper.scm
+++ assignments/la1-1/super-duper.scm
@@ -1,17 +1,51 @@
 ; Scheme copy program
 (define (copy source count)
-    (do ((i 1 (1+ i)))
-        ((i > count))
-        (display source)))
+        (make-list count source))
 
 (define (super-duper source count)
-    (if (null? source)
-        source
-        (if (number? source)
-            source
-        (copy source count))))
-        
-; (display (super-duper 123 2))
-; (display (super-duper '( ) 2))
-(display (super-duper '(x) 2))
+    (cond ((null? source) 
+           source)
+          ((number? source) 
+           source)
+          ((string? source) 
+           source)
+          ((pair? (list-ref source 0)) 
+           (append (copy (super-duper (list-ref source 0) count) count) (super-duper (cdr source) count)))
+          (else (append (copy (car source) count) (super-duper (cdr source) count)))))
+
+; Test cases
+;; Numbers
+(display (super-duper 123 1))
+(display "\n")
+(display (super-duper 123 2))
+(display "\n")
+
+;; Number List
+(display (super-duper '(123 123) 2))
+(display "\n")
+
+;; Empty list
+(display (super-duper '() 2))
+(display "\n")
+
+;; Normal List
+(display (super-duper '(x y) 1))
+(display "\n")
+(display (super-duper '(x y) 2))
+(display "\n")
+
+;; Lists within a list (including empty lists)
+(display (super-duper '((a b) y) 3))
+(display "\n")
+(display (super-duper '(( ) y) 3))
+(display "\n")
+(display (super-duper '((a b) (c d) y) 3))
+(display "\n")
+(display (super-duper '(x (c d) y) 2))
+(display "\n")
+(display (super-duper '(x ( ) y) 2))
+(display "\n")
+(display (super-duper '(x y (d e)) 2))
+(display "\n")
+(display (super-duper '(x y (d (e))) 2))
 (display "\n")
