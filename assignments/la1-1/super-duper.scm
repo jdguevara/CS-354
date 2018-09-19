7e4da454bfdd6098d2d24a7c475ee3da3dc88d3d
Jaime D. Guevara <jguevara@jaimes-air.boisestate.edu>
2018-09-19 16:55:30 -0600

    Added more cases and checks for booleans, symbols, and characters

--- assignments/la1-1/super-duper.scm
+++ assignments/la1-1/super-duper.scm
@@ -1,11 +1,17 @@
 ; Scheme copy program
 (define (copy source count)
-    (cond ((null? source) 
-           source)
-          ((number? source) 
-           source)
-          ((string? source) 
-           source)
+    (cond ((number? source)
+            (make-list 1 source))
+          ((null? source)
+            (make-list 1 source))
+          ((string? source)
+            (make-list 1 source))
+          ((boolean? source)
+            (make-list 1 source))
+          ((symbol? source)
+            (make-list 1 source))
+          ((char? source)
+            (make-list 1 source))
           (else (make-list count source))))
 
 (define (super-duper source count)
@@ -15,9 +21,17 @@
            source)
           ((string? source) 
            source)
+          ((boolean? source)
+           source)
+          ((symbol? source)
+           source)
+          ((char? source)
+           source)
           ((pair? (list-ref source 0)) 
-           (cons (copy (super-duper (list-ref source 0) count) count) (super-duper (cdr source) count)))
-          (else (cons (copy (car source) count) (super-duper (cdr source) count)))))
+           (append (copy (super-duper (list-ref source 0) count) count) (super-duper (cdr source) count)))
+          (else (append (copy (car source) count) (super-duper (cdr source) count)))))
+
+
 
 ; Test cases
 ;; Numbers
@@ -25,10 +39,30 @@
 (display "\n")
 (display (super-duper 123 2))
 (display "\n")
-
-;; Numbers in a List
 (display (super-duper '(123 123) 2))
 (display "\n")
+(display (super-duper '(x 123 y) 2))
+(display "\n")
+(display (super-duper '(x (123) y) 2))
+(display "\n")
+
+;; Booleans
+(display (super-duper #t 2))
+(newline)
+(display (super-duper '(#t) 2))
+(newline)
+(display (super-duper '(x #t y) 2))
+(newline)
+(display (super-duper '(x (#t) y) 2))
+(newline)
+
+;; Symbols
+(display (super-duper 'a 2))
+(newline)
+
+;; Characters
+(display (super-duper #\A 2 ))
+(newline)
 
 ;; Empty list
 (display (super-duper '() 2))
