de1acfd2d16d92335e54c512c0f228c2b3cc013d
Jaime Guevara <jaimeguevara@u.boisestate.edu>
2018-11-06 16:00:02 -0700

    Put the up-to-date program into just meet.pl, updated MAKE to run and compile just meet.pl, and added README

--- assignments/la3/meet.pl
+++ assignments/la3/meet.pl
@@ -1,10 +1,12 @@
 :- include('data.pl').
 
-people([ann,bob,carla]).
+meet(Person, slot(time(U,V),time(X,Y))) :-
+	free(Person,slot(time(A,B),time(D,E))),
+	J is U+(V/100), K is X+(Y/100),
+	L is A+(B/100), M is D+(E/100),
+	L=<J,
+	M>=K.
 
-main :- (setof(Slot,meet(Slot),Slots); halt),
-        write(Slots),
-        nl,
-        halt.
+main :- meet(Person,slot(time(8,30),time(8,45))),write(Person),nl,halt.
 
 :- initialization(main).
--- assignments/la3/meetone.pl
+++
@@ -1,8 +0,0 @@
-:- include('data.pl').
-
-main :- (meetone(Person,slot(time(8,30),time(8,45))); halt),
-	write(Person),
-        nl,
-        halt.
-
-:- initialization(main).
