0fa3df60c069355dfcd8cf78b09ed46914a0c110
Jaime Guevara <jaimeguevara@u.boisestate.edu>
2018-11-07 19:20:17 -0700

    Update for Prolog

--- assignments/la3/meet.pl
+++ assignments/la3/meet.pl
@@ -1,12 +1,12 @@
 :- include('data.pl').
 
-meet(Person, slot(time(U,V),time(X,Y))) :-
+meet(Person, slot(time(U,V,W),time(X,Y,Z))) :-
 	free(Person,slot(time(A,B),time(D,E))),
-	J is U+(V/100), K is X+(Y/100),
-	L is A+(B/100), M is D+(E/100),
+	J is V+(U*60), K is Y+(X*60),
+	L is B+(A*60), M is E+(D*60),
 	L=<J,
 	M>=K.
 
-main :- meet(Person,slot(time(8,30),time(8,45))),write(Person),nl,halt.
+main :- meet(Person,slot(time(8,30,am),time(8,45,am))),write(Person),nl,halt.
 
 :- initialization(main).
