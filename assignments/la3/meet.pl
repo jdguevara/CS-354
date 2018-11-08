681a5c0ee7a8c186ecaba789f7a07c3dfef19a22
Jaime Guevara <jaimeguevara@u.boisestate.edu>
2018-11-07 19:40:22 -0700

    Final LA3

--- assignments/la3/meet.pl
+++ assignments/la3/meet.pl
@@ -1,12 +1,13 @@
 :- include('data.pl').
 
-meet(Person, slot(time(U,V,W),time(X,Y,Z))) :-
+meet(Person, slot(time(U,V),time(X,Y))) :-
 	free(Person,slot(time(A,B),time(D,E))),
 	J is V+(U*60), K is Y+(X*60),
 	L is B+(A*60), M is E+(D*60),
 	L=<J,
 	M>=K.
 
-main :- meet(Person,slot(time(8,30,am),time(8,45,am))),write(Person),nl,halt.
+main :- meet(Person,slot(time(8,30),time(8,45))),write(Person),nl,halt.
 
 :- initialization(main).
+
